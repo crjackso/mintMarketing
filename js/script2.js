7e09b09dc2339cf9ebe2932f061ced38c08cdabc
Chris Jackson <crjackso@gmail.com>
2013-02-05 21:29:29 -0600

    Adding left/right keyboard control for leadership slide

--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,53 @@
 
+var _presentationManager = {};
+
 (function(a){function p(a){if((new Date).getTime()<d+c)return;d=(new Date).getTime();var b=f.offset()!=null?f.offset().left:0,i=f.offset()!=null?f.offset().top:0,j=a.pageX-b,k=a.pageY-i;if(j<0||j>f.width()||k<0||k>f.height())return;if(g==true){var l=window.orientation?(window.orientation+180)%360/90:2,m=a.accelerationIncludingGravity,n=l%2==0?-m.x:m.y,o=l%2==0?m.y:m.x;j=l>=2?n:-n;k=l>=2?o:-o;j=(j+h)/2;k=(k+h)/2;if(j<0){j=0}else if(j>h){j=h}if(k<0){k=0}else if(k>h){k=h}}var p=j/(g==true?h:f.width()),q=k/(g==true?h:f.height()),r,l;for(l=e.length;l--;){r=e[l];newX=r.startX+r.inversionFactor*r.xRange*p;newY=r.startY+r.inversionFactor*r.yRange*q;if(r.background){r.obj.css("background-position",newX+"px "+newY+"px")}else{r.obj.css("left",newX).css("top",newY)}}}function o(b){if((new Date).getTime()<d+c)return;if(n()){var e=b.accelerationIncludingGravity,f=e.x,o=e.y;if(k.xArray.length>=5){k.xArray.shift()}if(k.yArray.length>=5){k.yArray.shift()}k.xArray.push(f);k.yArray.push(o);k.xMotion=Math.round((m(k.xArray)-l(k.xArray))*1e3)/1e3;k.yMotion=Math.round((m(k.yArray)-l(k.yArray))*1e3)/1e3;if(k.xMotion>1.5||k.yMotion>1.5){if(h!=10){h=10}}if(k.xMotion>i||k.yMotion>i){j++}else{j=0}if(j>=5){g=true;a(document).unbind("mousemove.plax");a(window).bind("devicemotion",p(b))}else{g=false;a(window).unbind("devicemotion");a(document).bind("mousemove.plax",function(a){p(a)})}}}function n(){return window.DeviceMotionEvent!=undefined}function m(a){return Math.max.apply({},a)}function l(a){return Math.min.apply({},a)}var b=25,c=1/b*1e3,d=(new Date).getTime(),e=[],f=a(window),g=false,h=1,i=.05,j=0,k={xArray:[0,0,0,0,0],yArray:[0,0,0,0,0],xMotion:0,yMotion:0};a.fn.plaxify=function(b){return this.each(function(){var c=-1;var d={xRange:a(this).data("xrange")||0,yRange:a(this).data("yrange")||0,invert:a(this).data("invert")||false,background:a(this).data("background")||false};for(var f=0;f<e.length;f++){if(this===e[f].obj.get(0)){c=f}}for(var g in b){if(d[g]==0){d[g]=b[g]}}d.inversionFactor=d.invert?-1:1;d.obj=a(this);if(d.background){pos=(d.obj.css("background-position")||"0px 0px").split(/ /);if(pos.length!=2){return}x=pos[0].match(/^((-?\d+)\s*px|0+\s*%|left)$/);y=pos[1].match(/^((-?\d+)\s*px|0+\s*%|top)$/);if(!x||!y){return}d.startX=x[2]||0;d.startY=y[2]||0}else{var h=d.obj.position();d.obj.css({top:h.top,left:h.left,right:"",bottom:""});d.startX=this.offsetLeft;d.startY=this.offsetTop}d.startX-=d.inversionFactor*Math.floor(d.xRange/2);d.startY-=d.inversionFactor*Math.floor(d.yRange/2);if(c>=0){e.splice(c,1,d)}else{e.push(d)}})};a.plax={enable:function(b){a(document).bind("mousemove.plax",function(c){if(b){f=b.activityTarget||a(window)}p(c)});if(n()){window.ondevicemotion=function(a){o(a)}}},disable:function(){a(document).unbind("mousemove.plax");window.ondevicemotion=undefined}};if(typeof ender!=="undefined"){a.ender(a.fn,true)}})(function(){return typeof jQuery!=="undefined"?jQuery:ender}());
 
 (function(a){function d(b){var c=b||window.event,d=[].slice.call(arguments,1),e=0,f=!0,g=0,h=0;return b=a.event.fix(c),b.type="mousewheel",c.wheelDelta&&(e=c.wheelDelta/120),c.detail&&(e=-c.detail/3),h=e,c.axis!==undefined&&c.axis===c.HORIZONTAL_AXIS&&(h=0,g=-1*e),c.wheelDeltaY!==undefined&&(h=c.wheelDeltaY/120),c.wheelDeltaX!==undefined&&(g=-1*c.wheelDeltaX/120),d.unshift(b,e,g,h),(a.event.dispatch||a.event.handle).apply(this,d)}var b=["DOMMouseScroll","mousewheel"];if(a.event.fixHooks)for(var c=b.length;c;)a.event.fixHooks[b[--c]]=a.event.mouseHooks;a.event.special.mousewheel={setup:function(){if(this.addEventListener)for(var a=b.length;a;)this.addEventListener(b[--a],d,!1);else this.onmousewheel=d},teardown:function(){if(this.removeEventListener)for(var a=b.length;a;)this.removeEventListener(b[--a],d,!1);else this.onmousewheel=null}},a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})})(jQuery);
 
+mint.PresentationManager = function(){
+    var self, leadershipSlider;
+    self = this;
+
+    self.goToNextLeadershipSlide = function() {
+        leadershipSlider.goToNextSlide();
+        return false;
+    };
+
+    self.goToPreviousLeadershipSlide = function(){
+        leadershipSlider.goToPrevSlide();
+        return false;
+    };
+
+    self.init = function() {
+
+        leadershipSlider = $('.leadership-slider').bxSlider({
+            infiniteLoop: false,
+            controls: false,
+            hideControlOnEnd: true,
+            slideMargin: 10,
+            pager: false
+        });
+
+        $('#leadership-learn-more').click(function(){
+            return self.goToNextLeadershipSlide();
+        });
+
+        $('#leadership-slider-prev').click(function(){
+            return self.goToPreviousLeadershipSlide();
+        });
+    };
+
+    return self;
+};
+
 $(document).ready(function () {
 
     window.audioPlayed = false;
+
+    _presentationManager = new mint.PresentationManager();
+    _presentationManager.init();
+
     /****************************************************************************************
     Process Window Scroll Movements
     ***************************************************************************************/
@@ -260,24 +302,6 @@ $(document).ready(function () {
         });
     });
 
-    var slider = $('.bxslider').bxSlider({
-        infiniteLoop: false,
-        controls: false,
-        hideControlOnEnd: true,
-        slideMargin: 10,
-        pager: false
-    });
-
-    $('#leadership-learn-more').click(function(){
-        slider.goToNextSlide();
-        return false;
-    });
-
-    $('#leadership-slider-prev').click(function(){
-        slider.goToPrevSlide();
-        return false;
-    });
-
     $(".services").touchSlider({
         duration: 350,
         mouseTouch: true,
@@ -348,15 +372,10 @@ $(document).ready(function () {
 });
 
 
-
 var currentScreen = 0;
 var scrollReady = false;
 var scrollLocked = false;
 var scrollDirection = 'down';
-var MIN_HEIGHT = 600;
-var screenHeight = $(window).height() > MIN_HEIGHT ? $(window).height() : MIN_HEIGHT;
-var documentWrapWidth = 1100;
-var documentGutterWidth = (($(document).width() - documentWrapWidth) / 2);
 var screenAnimationTime = 1800;
 var screens = mint.screens;
 
@@ -422,18 +441,26 @@ function setupKeyScrollHandler() {
                 }
             }
         }
+        if($("#leadership").is(":within-viewport")){
+            if(event.keyCode == 39){
+                _presentationManager.goToNextLeadershipSlide();
+            }
+            else if(event.keyCode == 37){
+                _presentationManager.goToPreviousLeadershipSlide();
+            }
+        }
     });
     $(document).bind("keydown", function (event) {
-        if (event.keyCode == 40 || event.keyCode == 38 ) {
-            event.preventDefault()
+        if (event.keyCode == 40 || event.keyCode == 38) {
+            event.preventDefault();
         }
     })
 }
 // Bind and unbind keys for form elements
-function unbindKeys() {
-    $(document).unbind("keyup");
-    $(document).unbind("keydown");
-}
-$("input, textarea, select").live("focus", function () { unbindKeys(); });
-$("input, textarea, select").live("blur", function () {setupKeyScrollHandler(); });
+//function unbindKeys() {
+//    $(document).unbind("keyup");
+//    $(document).unbind("keydown");
+//}
+//$("input, textarea, select").live("focus", function () { unbindKeys(); });
+//$("input, textarea, select").live("blur", function () {setupKeyScrollHandler(); });
 
