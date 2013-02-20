5b457eb549e47f59b39ab122f70d4c6cdc2f8748
Chris Jackson <crjackso@gmail.com>
2013-02-19 20:07:25 -0600

    Continued trial of queryLoader for pre-loading images

--- js/loader.js
+++ js/loader.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     $("body").queryLoader2({
 //            barColor: "#6e6d73",
-        backgroundColor: "#fff1b0",
+//        backgroundColor: "#fff1b0",
         percentage: true,
 //            barHeight: 30,
         completeAnimation: "grow"
