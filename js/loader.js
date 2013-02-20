eedbca4ee67798c0bdf7510937196d57fb2e0403
Chris Jackson <crjackso@gmail.com>
2013-02-19 22:37:14 -0600

    Changing placement of script inclusion

--- js/loader.js
+++ js/loader.js
@@ -5,10 +5,7 @@ $(document).ready(function () {
     }
 
     $("body").queryLoader2({
-//            barColor: "#6e6d73",
-//        backgroundColor: "#fff1b0",
         percentage: true,
-//            barHeight: 30,
         completeAnimation: "grow"
     });
 });
