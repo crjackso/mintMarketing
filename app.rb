d765714979e17bc57339e4b937abc9029b16a887
Chris Jackson <crjackso@gmail.com>
2013-02-26 00:16:19 -0600

    Switching to Sinatra app

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 
+before do
+  headers "Content-Type" => "text/html; charset=utf8"
+end
+
+helpers do
+  def cache_for(time)
+    response['Cache-Control'] = "public, max-age=#{time.to_i}"
+  end
+end
+
 get '/' do
-  redirect '/presentation.html'
+  cache_for 10*60
+  redirect 'general.html'
 end
 
 get '/gm' do
-  redirect '/gm.html'
+  redirect 'gm.html'
 end
